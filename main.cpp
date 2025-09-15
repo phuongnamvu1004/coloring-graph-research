de3f8137b9591b58320da4eaf83d9680032df6f2
flyingpenguin223 <yadel.adelie@gmail.com>
2025-09-15 11:13:05 -0400

    make generating graph clearer (easier to iterate over simple trees)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <algorithm>
+#include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/compressed_sparse_row_graph.hpp>
 #include <iostream>
 
@@ -9,13 +9,21 @@
 #include "eigen/Eigen/Dense"
 
 int main() {
-	Edge special_graph_edges[] = {
-		Edge(0, 1),
-		Edge(1, 2),
-	};
+	// Edge special_graph_edges[] = {
+	// 	Edge(0, 1),
+	// 	Edge(1, 2),
+	// };
+	//
+	// int number_of_vertices = 3;
+	// Graph original_graph(special_graph_edges, special_graph_edges + sizeof(special_graph_edges)/sizeof(Edge), number_of_vertices);
+	Graph original_graph;
 
-	int number_of_vertices = 3;
-	Graph original_graph(special_graph_edges, special_graph_edges + sizeof(special_graph_edges)/sizeof(Edge), number_of_vertices);
+	auto v1 = boost::add_vertex(original_graph);
+	auto v2 = boost::add_vertex(original_graph);
+	auto v3 = boost::add_vertex(original_graph);
+
+	boost::add_edge(v1, v2, original_graph);
+	boost::add_edge(v2, v3, original_graph);
 
 	int k = 3;
 
@@ -27,13 +35,11 @@ int main() {
 	std::vector<int> n_complete_non_reconstructible_non_special_vertices;
 	Graph coloring_graph = coloringFromOriginal(original_graph, k, adj_list, special_vertex_classes, class_to_num_vertices, special_vertices, reconstructible_vertices);
 
-	graph_traits<Graph>::edge_iterator ei, ei_end;
-
-
 	int num_coloring_vertices = coloring_graph.m_vertices.size();
 
 	Eigen::MatrixXd adjacency_matrix = Eigen::MatrixXd::Zero(num_coloring_vertices, num_coloring_vertices);
 
+	graph_traits<Graph>::edge_iterator ei, ei_end;
 	for (tie(ei, ei_end) = edges(coloring_graph); ei != ei_end; ++ei) { // loop through each edge
 		auto u = source(*ei, coloring_graph); // vertex label of "source"
 		auto v = target(*ei, coloring_graph); // vertex label of "destination"
