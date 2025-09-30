5bfa26a3637882c96b53315442eb4bb1916fdaa1
flyingpenguin223 <yadel.adelie@gmail.com>
2025-09-30 18:43:21 -0400

    add zlm import (no eigenvalues builtin though :(

--- zig-graph/build.zig
+++ zig-graph/build.zig
@@ -4,13 +4,17 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const zlm = b.dependency("zlm", .{});
+
     const exe = b.addExecutable(.{
         .name = "zig_graph",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
-
             .target = target,
             .optimize = optimize,
+            .imports = &.{
+                .{ .name = "zlm", .module = zlm.module("zlm") },
+            },
         }),
     });
 
--- zig-graph/src/main.zig
+++ zig-graph/src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 const Graph = @import("graph.zig");
+const zlm = @import("zlm").as(f64);
 
 pub fn main() !void {
     var allocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -14,13 +15,13 @@ pub fn main() !void {
     const v1 = try graph.add_vertex(1);
     const v2 = try graph.add_vertex(2);
     const v3 = try graph.add_vertex(3);
-    // const v4 = try graph.add_vertex(3);
+    const v4 = try graph.add_vertex(3);
     // const v5 = try graph.add_vertex(3);
     // const v6 = try graph.add_vertex(3);
 
     _ = try graph.add_edge(v1, v2);
     _ = try graph.add_edge(v2, v3);
-    // _ = try graph.add_edge(v3, v4);
+    _ = try graph.add_edge(v3, v4);
     // _ = try graph.add_edge(v4, v5);
     // _ = try graph.add_edge(v5, v6);
     // _ = try graph.add_edge(v6, v1);
@@ -29,7 +30,7 @@ pub fn main() !void {
     // if (graph.is_coloring_valid(&.{ 1, 2, 2, 2 }))
     //     std.debug.print("valid\n", .{});
 
-    const k = 3;
+    const k = 4;
 
     std.debug.print("generating coloring graph...\n", .{});
     var coloring_graph = try graph.get_coloring_graph(k, gpa);
